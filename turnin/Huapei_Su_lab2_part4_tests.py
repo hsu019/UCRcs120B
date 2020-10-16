6fa6d00461459a2e435ff5324166db9679755123
hsu019 <59679803+hsu019@users.noreply.github.com>
2020-10-17 07:07:14 +0800

    Update Huapei_Su_lab2_part4_tests.py

--- turnin/Huapei_Su_lab2_part4_tests.py
+++ turnin/Huapei_Su_lab2_part4_tests.py
@@ -1,54 +1,35 @@
-/*	Author:  hsu019 Huapei Su
- *  Partner(s) Name: none
- *	Lab Section: 023
- *	Assignment: Lab #2  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ {'description': 'This test will run first.',
+    'steps': [ {'inputs': [('PINA',0x00),('PINB',0x00),('PINC',0x00)], 'iterations': 1 } ],
+    'expected': [('PORTD',0x04)],
+    },
+    {'description': 'This test will run second.',
+    'steps': [ 
+        {'inputs': [('PINA',0x10),('PINB',0x10),('PINC',0x10)], 'iterations': 1}, ],
+    'expected': [('PORTD',0xC1)],
+    },
+    {'description': 'This is test will run third.',
+    'steps': [ {'inputs': [('PINA',0x11),('PINB',0x00),('PINC',0x04)], 'iterations': 1 } ],
+    'expected': [('PORTD',0x86)],
+    },
+    ]
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as outputs, initialize to 0s
-	DDRC = 0x00; PORTC = 0xFF; // Configure port C's 8 pins as inputs
-	DDRD = 0xFF; PORTD = 0x00; // Configure port D's 8 pins as outputs, initialize to 0s
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-	unsigned char tmpB = 0x00;
-	unsigned char tmpC = 0x00;
-	unsigned char tmpD = 0x00;
-	unsigned char total = 0x00;
-while(1) {
-		tmpA = PINA;
-		tmpB = PINB;
-		tmpC = PINC;
-
-		total = tmpA + tmpB + tmpC;
-
-		if (total > 140) { 
-			tmpD = (tmpD & 0xFE) | 0x01;
-		}
-
-		if (tmpA > tmpB)
-		{
-			if((tmpA - tmpB) > 80){
-				tmpD = (tmpD & 0xFD) | 0x02;
-			}
-		}
-		else{
-			if((tmpB - tmpA) > 80){
-				tmpD = (tmpD & 0xFD) | 0x02;
-			}
-		}
-		
-		tmpD = (tmpD & 0x03) | (total & 0xFC);
-		
-		PORTD = tmpD;	
-	}
-    return 1;
-}
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+watch = ['main::tmpA,tmpB,tmpC','PORTD']
