399c6910003b1957b6d18f6ab9b03dbc3494f088
Huapei Su <hsu019@ucr.edu>
2020-10-16 17:48:12 -0700

    Completed part 4

---
+++ turnin/Huapei_Su_lab3_part4.c
@@ -0,0 +1,35 @@
+/*	Author:  hsu019 Huapei Su
+ *  Partner(s) Name: none
+ *	Lab Section: 023
+ *	Assignment: Lab #3  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; 
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00; 
+	unsigned char tmpA = 0x00; 
+	unsigned char upper;
+	unsigned char lower;
+while(1) {
+		tmpA = PINA;
+		upper = 0x00;
+		lower = 0x00;
+
+		lower = tmpA >> 4;
+		upper = tmpA << 4;
+
+		PORTB = lower;
+		PORTC = upper;	
+	}
+    return 1;
+}
